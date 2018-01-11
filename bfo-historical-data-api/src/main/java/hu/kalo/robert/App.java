b9d1ddbcf089a71346d5f8a55ed26a5a672c7c3b
Kalo Robert <robert.kalo@gmail.com>
2018-01-11 20:17:04 +0100

    First commit

---
+++ bfo-historical-data-api/src/main/java/hu/kalo/robert/App.java
@@ -0,0 +1,13 @@
+package hu.kalo.robert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
